91f4cbe63c20f9e686e37a5e42f545a1c2278fc0
Harry <harry90enpi@gmail.com>
2019-01-27 12:43:14 +0100

    did all the things

--- address_book/models.py
+++ address_book/models.py
@@ -3,11 +3,12 @@ from django.db import models
 class Address(models.Model):
 	name = models.CharField(max_length=200)
 	email = models.EmailField(max_length=200)
-	phone = models.CharField(max_length=20)
+	phone = models.CharField(max_length=15)
 	address = models.CharField(max_length=200)
 	city = models.CharField(max_length=200)
-	state = models.CharField(max_length=200)
+	state = models.CharField(max_length=100)
 	zipcode = models.CharField(max_length=10)
 
+	
 	def __str__(self):
 		return self.name
